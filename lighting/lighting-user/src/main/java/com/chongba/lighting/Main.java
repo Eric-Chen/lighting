763e6b1b210dd6e642e0e86edd87e8980529411f
eric <ct@kuaihuoyun.com>
2019-10-17 16:52:51 +0800

    init

---
+++ lighting/lighting-user/src/main/java/com/chongba/lighting/Main.java
@@ -0,0 +1,14 @@
+package com.chongba.lighting;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.mongo.MongoAutoConfiguration;
+
+@SpringBootApplication(scanBasePackages = {"com.chongba.lighting.controller"}, exclude = {MongoAutoConfiguration.class})
+public class Main {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Main.class, args);
+    }
+
+}
